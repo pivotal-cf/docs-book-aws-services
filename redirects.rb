298feb166aeaa17317660aae2651ab0693e8a5ba
Samia Nneji <snneji@vmware.com>
2022-05-17 11:50:49 +0100

    Fix redirects [#182195205]

--- redirects.rb
+++ redirects.rb
@@ -5,7 +5,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 # Redirect from docs.pivotal.io/aws-services/ to docs.vmware.com VMware Tanzu Service Broker for AWS docs
 
-r301  %r{/aws-services/index.html}, "https://docs.vmware.com/en/VMware-Tanzu-Service-Broker-for-AWS/1.4/documentation/GUID-index.html"
-r301  %r{/aws-services/}, "https://docs.vmware.com/en/VMware-Tanzu-Service-Broker-for-AWS/1.4/documentation/GUID-index.html"
-r301  %r{/aws-services}, "https://docs.vmware.com/en/VMware-Tanzu-Service-Broker-for-AWS/1.4/documentation/GUID-index.html"
 r301  %r{/aws-services/(.*)}, "https://docs.vmware.com/en/VMware-Tanzu-Service-Broker-for-AWS/1.4/documentation/GUID-$1"
